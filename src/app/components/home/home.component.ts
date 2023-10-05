a1b96bfbc330e7847210ffae1465da3b2fa0e01a
kjdv91 <126703713+kjaramillo91@users.noreply.github.com>
2023-10-04 20:33:08 -0700

    interactive

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,24 +7,71 @@ import { Component, ViewEncapsulation, ElementRef, Renderer2 } from '@angular/co
   encapsulation: ViewEncapsulation.None, // Cambia aquí a None
 })
 export class HomeComponent {
+
+  imagenes = [
+    {
+      principal: '../../../assets/productsHome/ElefantesJuego.png',
+      secundaria: '../../../assets/productsHome/ElefantesJuegoBack.png',
+      hover: false
+    },
+    {
+      principal: '../../../assets/productsHome/GallinaHuevera.png',
+      secundaria: '../../../assets/productsHome/henBack.png',
+      hover: false
+    },
+    {
+      principal: '../../../assets/productsHome/JuegoBudas.png',
+      secundaria: '../../../assets/productsHome/budasGameBack.png',
+      hover: false
+    },
+    {
+      principal: '../../../assets/productsHome/JuegoGirasoles.png',
+      secundaria: '../../../assets/productsHome/JuegoGirasolesBack.png',
+      hover: false
+    }
+  ];
+
+  imagenesTwo = [
+    {
+      principal: '../../../assets/productsHome/Vaquita.jpg',
+      secundaria: '../../../assets/productsHome/backCow.png',
+      hover: false
+    },
+    {
+      principal: './../../assets/productsHome/elephantsFront.png',
+      secundaria: '../../../assets/productsHome/elephantsBack.png',
+      hover: false
+    },
+    {
+      principal: '../../../assets/productsHome/Pony&Perro.png',
+      secundaria: '../../../assets/productsHome/backPony&Dog.png',
+      hover: false
+    },
+    {
+      principal: '../../../assets/productsHome/DosMorenas.png',
+      secundaria: '../../../assets/productsHome/brunettesBack.png',
+      hover: false
+    }
+  ];
+
+
   constructor(
     private elRef: ElementRef,
     private renderer: Renderer2
 
   ){}
 
-  // ngAfterViewInit() {
-  //   const imgProducts = this.elRef.nativeElement.querySelectorAll('.img-product');
+    ngOnInit(){
 
-  //   imgProducts.forEach(product => {
-  //     this.renderer.listen(product, 'click', () => {
-  //       const img = product.querySelector('img');
-  //       if (img) {
-  //         this.renderer.setStyle(img, 'transform', 'scale(1.2)');
-  //       }
-  //     });
-  //   });
-  // }
+    }
+
+    cambiarEstadoImg1(index: number) {
+      this.imagenes[index].hover = !this.imagenes[index].hover;
+    }
+
+    cambiarEstadoImg2(index: number) {
+      this.imagenesTwo[index].hover = !this.imagenesTwo[index].hover;
+    }
 
 
 
